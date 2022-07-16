54daed2a233f7df4f116001fdb051f0dffb0b673
Lucio Schiavoni <luciosc1798@gmail.com>
2022-07-15 21:24:40 -0300

    Entrega-3

---
+++ main.js
@@ -0,0 +1,42 @@
+class Usuario{
+    constructor(nombre, email, constraseña, capital, porcentaje, apalancamiento){
+        this.nombre = nombre
+        this.email = email
+        this.constraseña = constraseña
+        this.capital = capital
+        this.apalancamiento = apalancamiento
+        this.porcentaje = porcentaje
+    }
+    
+    }
+    
+    const usuarios = []
+    
+    const form = document.getElementById("idForm")
+    
+    form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let nombre = document.getElementById("idNombre").value
+    let email= document.getElementById("idEmail").value
+    let constraseña = document.getElementById("idContraseña").value
+    let capital = document.getElementById("idCapital").value
+    let porcentaje = document.getElementById("idPorcentaje").value
+    let apalancamiento = document.getElementById("idApalancamiento").value
+    
+    const usuario = new Usuario(nombre, email, constraseña, capital,porcentaje, apalancamiento)
+    usuarios.push(usuario)
+    console.log(usuarios)
+    
+    
+    
+    })
+    
+    
+    
+    const ganancia = document.getElementById("botonGanancia")
+    
+    ganancia.addEventListener("click", () => {
+    
+    let operacion = parseInt((this.porcentaje*this.apalancamiento)*this.capital)/100
+    console.log("Su ganancia es de " +operacion+ "USD")
+    } )
